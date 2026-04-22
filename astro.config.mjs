8bc15378bc9a0bdc5978a962049c43073a2f8676
Javier Ayala <javierayala1618@gmail.com>
2026-04-22 14:13:05 -0500

    Added components for inscription table and updated other sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://asonorterx.netlify.app",
+
   vite: {
     plugins: [tailwindcss()],
   },
